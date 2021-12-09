0bb926fe564b12f1d5f65e2ed8bc68b3c06d9834
awaisaasi <contact@awaisaasi.com>
2021-12-09 17:46:08 +0500

    footer

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,6 +8,8 @@ module.exports = {
     extend: {
       backgroundImage: {
         back: "linear-gradient(rgba(0, 0, 0, 0.8), rgba(1, 0, 0, 0.3)), url(https://images.pexels.com/photos/280229/pexels-photo-280229.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500)",
+        backa:
+          "url(https://cdn.pixabay.com/photo/2017/01/22/18/14/hamburg-2000424__340.jpg)",
       },
       fontFamily: {
         sans: ["Inter", ...defaultTheme.fontFamily.sans],
